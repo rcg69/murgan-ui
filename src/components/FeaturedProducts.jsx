9a6ec19904946fa0e649461e3ae145cc1f8febd2
Ram Charan G Gaddamidhi <ramgoud696@gmail.com>
2026-03-04 20:22:10 +0530

    added obnclick for featured collection images
    added obnclick for featured collection images

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,18 +1,25 @@
+import Link from "next/link";
 import "../styles/FeaturedProducts.css";
 
-export default function FeaturedProducts() {
+export default function FeaturedProducts({ products }) {
   return (
     <section className="featured-section">
       {/* Section Header */}
       <div className="featured-header">
         <h2>Featured Collection</h2>
-  {/*       <p>Curated editorial picks for the season</p> */}
+        {/* <p>Curated editorial picks for the season</p> */}
       </div>
 
       {/* Editorial Grid */}
       <div className="featured-grid">
         <div className="featured-item large">
-          <img src="/saree.png" alt="Product" />
+          <Link href="/products">
+            <img
+              src="/saree.png"
+              alt="Product"
+              className="cursor-pointer"
+            />
+          </Link>
           <span>THE ART OF STILLNESS</span>
         </div>
 
@@ -43,7 +50,7 @@ export default function FeaturedProducts() {
       </div>
 
       {/* CTA */}
-{/*       <div className="featured-cta">
+      {/* <div className="featured-cta">
         <button>View Full Collection</button>
       </div> */}
     </section>
